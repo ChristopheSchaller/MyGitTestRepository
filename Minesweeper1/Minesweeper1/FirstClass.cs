6929b99fca190db4bf7221bdb9b7f1e51c9493a1
ChristopheSchaller <chri.schaller@gmail.com>
2015-12-23 13:51:50 +0100

    Commit firstClass

---
+++ Minesweeper1/Minesweeper1/FirstClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper1
+{
+    /// <summary>
+    /// My first class.
+    /// </summary>
+    class FirstClass
+    {
+    }
+}
