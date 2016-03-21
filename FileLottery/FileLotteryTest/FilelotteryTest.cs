8b54c1c9f50eef4cbe2af2eb335de4029b9cfee5
ChristopheSchaller <chri.schaller@gmail.com>
2016-03-21 17:04:02 +0100

    Add file lottery to my git test repository.

---
+++ FileLottery/FileLotteryTest/FilelotteryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileLottery;
+
+namespace FileLotteryTest
+{
+    [TestClass]
+    public class FilelotteryTest
+    {
+        private Lottery _target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _target = new Lottery();
+        }
+
+        [TestMethod]
+        public void OnlyOneFileInTheDirectory_NextPath_shouldReturnTheOnlyFileFromTheDirectory()
+        {
+        }
+    }
+}
